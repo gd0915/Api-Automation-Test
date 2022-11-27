886cd3b82903dc56e5e7f655e89d004f3b12042d
Hatice Erzincan Sozeri <haticeesozeri@gmail.com>
2022-11-26 22:21:38 -0500

    New test files added

--- src/test/java/test_data/JsonPlaceHolderTestData.java
+++ src/test/java/test_data/JsonPlaceHolderTestData.java
@@ -20,5 +20,16 @@ public class JsonPlaceHolderTestData {
 
         return expectedData;
     }
+
+    public String expectedDataInString (Integer userId,String title,Boolean completed){
+        String expectedData ="{\n" +
+                "    \"userId\": "+userId+",\n" +
+                "    \"title\": \""+title+"\",\n" +
+                "    \"completed\": "+completed+"\n" +
+                "  }";
+
+        return expectedData;
+    }
+
 }
 
