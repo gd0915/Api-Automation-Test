db20a4dc52228d755c1cb7e5688406a38ad01a0d
Hatice Erzincan Sozeri <haticeesozeri@gmail.com>
2022-11-28 23:19:04 -0500

    New Test Files are added

---
+++ src/test/java/pojos/CountryPost.java
@@ -0,0 +1,44 @@
+package pojos;
+
+import org.codehaus.jackson.annotate.JsonIgnoreProperties;
+
+import java.util.ArrayList;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class CountryPost {
+
+    public String name;
+    public ArrayList<States> states;
+
+    public CountryPost(String name, ArrayList<States> states) {
+        this.name = name;
+        this.states = states;
+    }
+
+    public CountryPost() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<States> getStates() {
+        return states;
+    }
+
+    public void setStates(ArrayList<States> states) {
+        this.states = states;
+    }
+
+    @Override
+    public String toString() {
+        return "CountryPost{" +
+                "name='" + name + '\'' +
+                ", states=" + states +
+                '}';
+    }
+}
